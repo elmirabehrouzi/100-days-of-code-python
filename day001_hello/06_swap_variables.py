933292f187b215f8d80544aef8126137292dc207
elmirabehrouzi <behroozielmira1234@gmail.com>
2025-07-20 14:09:50 +0330

    Create 06_swap_variables.py

---
+++ day001_hello/06_swap_variables.py
@@ -0,0 +1,14 @@
+# Swapping the values of two variables
+
+# Get input from user
+a = input("a: ")
+b = input("b: ")
+
+# Use a temporary variable to swap values
+c = a
+a = b
+b = c
+
+# Print the results
+print("a: " + a)
+print("b: " + b)
